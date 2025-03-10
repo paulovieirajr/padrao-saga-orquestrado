a33e4f4b3120b295cb42e3528cd854059edf78b7
Paulo Vieira <paulovieiradev@outlook.com>
2025-03-10 14:40:28 -0300

    feat: created all producers

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/kafka/producer/SagaProducer.java
@@ -0,0 +1,35 @@
+package br.com.microservices.orchestrated.orderservice.core.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import static br.com.microservices.enums.Microservice.ORDER_SERVICE;
+import static br.com.microservices.log.CustomLog.FAILED_EVENT_LOG;
+import static br.com.microservices.log.CustomLog.SEND_EVENT_LOG;
+
+@Component
+public class SagaProducer {
+
+    private final static Logger LOG = LoggerFactory.getLogger(SagaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public SagaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            LOG.info(SEND_EVENT_LOG.getMessage(), ORDER_SERVICE.getMicroservice(), startSagaTopic, payload);
+            kafkaTemplate.send(startSagaTopic, payload);
+        } catch (Exception e) {
+            LOG.error(FAILED_EVENT_LOG.getMessage(), ORDER_SERVICE.getMicroservice(), startSagaTopic, payload, e);
+        }
+    }
+}
---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/kafka/producer/KafkaProducer.java
@@ -0,0 +1,35 @@
+package br.com.microservices.orchestrated.paymentservice.core.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import static br.com.microservices.enums.Microservice.PAYMENT_SERVICE;
+import static br.com.microservices.log.CustomLog.FAILED_EVENT_LOG;
+import static br.com.microservices.log.CustomLog.SEND_EVENT_LOG;
+
+@Component
+public class KafkaProducer {
+
+    private final static Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            LOG.info(SEND_EVENT_LOG.getMessage(), PAYMENT_SERVICE.getMicroservice(), orchestratorTopic, payload);
+            kafkaTemplate.send(orchestratorTopic, payload);
+        } catch (Exception e) {
+            LOG.error(FAILED_EVENT_LOG.getMessage(), PAYMENT_SERVICE.getMicroservice(), orchestratorTopic, payload, e);
+        }
+    }
+}
